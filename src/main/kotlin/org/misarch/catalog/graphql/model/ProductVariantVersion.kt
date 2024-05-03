d006a6792c6d3b0b4460b2d2ab832b745a4c7a4f
nk-coding <niklask.coding@gmail.com>
2024-05-03 17:58:34 +0200

    fix SQL query

--- src/main/kotlin/org/misarch/catalog/graphql/model/ProductVariantVersion.kt
+++ src/main/kotlin/org/misarch/catalog/graphql/model/ProductVariantVersion.kt
@@ -14,6 +14,7 @@ import org.misarch.catalog.graphql.model.connection.MediaConnection
 import org.misarch.catalog.graphql.model.connection.base.CommonOrder
 import org.misarch.catalog.persistence.model.CategoryCharacteristicValueEntity
 import org.misarch.catalog.persistence.model.CategoryEntity
+import org.misarch.catalog.persistence.model.ProductVariantVersionEntity
 import org.misarch.catalog.persistence.model.ProductVariantVersionToMediaEntity
 import org.misarch.catalog.persistence.repository.CategoryCharacteristicValueRepository
 import org.misarch.catalog.persistence.repository.MediaRepository
@@ -104,7 +105,7 @@ class ProductVariantVersion(
             dfe.authorizedUserOrNull
         ) {
             it.innerJoin(ProductVariantVersionToMediaEntity.ENTITY)
-                .on(ProductVariantVersionToMediaEntity.ENTITY.mediaId.eq(CategoryEntity.ENTITY.id))
+                .on(ProductVariantVersionToMediaEntity.ENTITY.productVariantVersionId.eq(ProductVariantVersionEntity.ENTITY.id))
         }
     }
 
